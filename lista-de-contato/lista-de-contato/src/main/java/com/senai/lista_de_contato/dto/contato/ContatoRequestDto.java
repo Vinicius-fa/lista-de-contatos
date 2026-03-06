04b0d2a58a26ca22ecdefb5afe900432e46cf7e1
VINÍCIUS DE FIGUEIREDO ANACLETO <vinicius_anacleto@estudante.sesisenai.org.br>
2026-03-06 19:16:07 -0300

    feat: validation to contatoRequest

--- lista-de-contato/lista-de-contato/src/main/java/com/senai/lista_de_contato/dto/contato/ContatoRequestDto.java
+++ lista-de-contato/lista-de-contato/src/main/java/com/senai/lista_de_contato/dto/contato/ContatoRequestDto.java
@@ -1,3 +1,16 @@
 package com.senai.lista_de_contato.dto.contato;
 
-public record ContatoRequestDto (String nome, String numero){ }
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record ContatoRequestDto (
+        @NotBlank(message = "O nome do contato é obrigatório")
+        @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
+        String nome,
+
+        @NotBlank(message = "O número de telefone é obrigatório")
+        @Pattern(regexp = "^\\(?\\d{2}\\)?\\s?\\d{4,5}-?\\d{4}$",
+                message = "O número deve estar no formato (XX) 9XXXX-XXXX ou XXXXXXXXXXX")
+        String numero
+){}
